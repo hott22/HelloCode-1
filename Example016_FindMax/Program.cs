05dc2b789e0a3335c824a598cce3f6b5fc918e45
Bobby <mif210210@gmail.com>
2021-11-24 05:02:48 +0300

    add Example016_FindMax

---
+++ Example016_FindMax/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти максимальное из трех чисел
+Console.Write("Введите первое число: ");
+double numA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double numB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите третье число: ");
+double numС = Convert.ToDouble(Console.ReadLine());
+
+double max = numA;
+if (numA > max) max = numA;
+if (numB > max) max = numB;
+if (numС > max) max = numС;
+
+Console.Write("Максимально число ");
+Console.WriteLine(max);
