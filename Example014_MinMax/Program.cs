a07bc5ac818aa2eb25c3ab2c2a0a9f75ae170575
Bobby <mif210210@gmail.com>
2021-11-24 04:56:21 +0300

    add Example014_MinMax

---
+++ Example014_MinMax/Program.cs
@@ -0,0 +1,21 @@
+﻿// Даны два числа. Показать большее и меньшее число
+Console.Write("Введите первое число: ");
+double numA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double numB = Convert.ToDouble(Console.ReadLine());
+
+if(numA < numB)
+{
+    Console.Write("Минимальное число: ");
+    Console.WriteLine(numA);
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(numB);
+}
+else
+{
+    Console.Write("Минимальное число: ");
+    Console.WriteLine(numB);
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(numA);
+}
+
